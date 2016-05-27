5d4ec33336ba28e985e70ef7d752bb1cce3adf3d
root <root@vps265630.ovh.net>
2016-05-27 22:58:09 +0200

    Varios ajustes realizados a estadisticas

--- src/IW/EasySurveyBundle/Controller/ResultController.php
+++ src/IW/EasySurveyBundle/Controller/ResultController.php
@@ -57,17 +57,65 @@ class ResultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $instance = $em->getRepository('IWEasySurveyBundle:Instance')->find($id);
         $quiz = $em->getRepository('IWEasySurveyBundle:Quiz')->find($instance->getQuizId());
+        $questions = $this->getIdQuestions($id);
+        
         return $this->render('IWEasySurveyBundle:Result:manage.html.twig', array('id'=>$id,'instanceName'=>$instance->getName(),'quizName'=>$quiz->getName()));        
     }
     
+    public function showStatisticAction ($id) 
+    {
+        $questions = $this->getIdQuestions($id);        
+        $em = $this->getDoctrine()->getManager();
+        $instance = $em->getRepository('IWEasySurveyBundle:Instance')->find($id);
+        
+        $result_text = array();
+        $result_numeric = array();
+        $result_simple = array();
+        $result_multiple = array();
+        foreach ($questions as $data) {
+                $answers = $em->getRepository('IWEasySurveyBundle:Answers')->findBy(array('idInstance'=>$id,'idQuestion'=>$data));
+                $question = $em->getRepository('IWEasySurveyBundle:Question')->find($data);
+                
+                switch($question->getTypeId()) {
+                            // Respuestas numericas
+                    case 0: 
+                            $result_numeric[] = array ('questionName'=>$question->getName(),'average'=>$this->getAverage($answers), 'median'=>$this->getMedian($answers),
+                                'mode'=>$this->getMode($answers), 'porcentages'=>$this->getPortentages($answers));
+                            break;
+                            // Respuestas de texto
+                    case 1: 
+                            $result_text[] = array ('questionName'=>$question->getName(),'responses'=>$this->getTextResponses($answers));
+                            break;
+                    // Respuestas de eleccion unica
+                    case 2:
+                            $result_simple[] = array ('questionName'=>$question->getName(), 'porcentages'=>$this->getPortentages($answers));
+                            break;
+                    // Respuestas de eleccion multiple
+                    case 3: 
+                            $result_multiple[] = array ('questionName'=>$question->getName(), 'porcentages'=>$this->getPortentages($answers));
+                            break;
+                }
+                
+            
+            
+        }
+        
+        return $this->render('IWEasySurveyBundle:Result:statistic.html.twig', 
+                array('name'=>$instance->getName(),
+                        'results_numeric'=>$result_numeric,
+                        'results_text'=>$result_text,
+                        'results_simple'=>$result_simple,
+                        'results_multiple'=>$result_multiple));
+    }
     
-   public function showDataAction($id) {
+   public function showDataAction($id) 
+    {
        $em = $this->getDoctrine()->getManager();
        $values = $this->getResults($id);
        $instance = $em->getRepository('IWEasySurveyBundle:Instance')->find($id);
        $quiz = $em->getRepository('IWEasySurveyBundle:Quiz')->find($instance->getQuizId());
        return $this->render('IWEasySurveyBundle:Result:result.html.twig', array('id'=>$id,'values'=>$values,'instanceName'=>$instance->getName(),'quizName'=>$quiz->getName()));        
-   }
+    }
 
 
    public function generateCsvAction($id)
@@ -80,5 +128,86 @@ class ResultController extends Controller
         return $response;
     }
     
+    private function getIdQuestions ($idInstance) 
+    {        
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            "SELECT DISTINCT(a.idQuestion)
+            FROM IWEasySurveyBundle:Answers a
+            WHERE a.idInstance = $idInstance "
+        );
+        
+        $idquestions = $query->getResult();
+        $results = array ();
+        foreach ($idquestions as $data) {
+            $results [] = $data[1];
+        }
+        
+        return $results;
+    }
     
+    private function getAverage($id)
+    {
+        $Average = 0;
+        foreach ($id as $data) {
+            $Average += $data->getBody();
+        }
+        $Average /= count($id);
+        
+        return $Average;
+    }    
+    
+    private function getMedian($id)
+    {
+        foreach ($id as $data){
+            $valuesArray[] = $data->getBody();
+        }
+        sort($valuesArray);
+        
+        $tam = count($valuesArray);
+        if ($tam%2 != 0) {
+            $median = $valuesArray[($tam/2)-(1/2)];
+        }
+        else {
+            $median = ($valuesArray[($tam/2)-1] + $valuesArray[($tam/2)])/2;
+        }
+        
+        return $median;
+    }
+    
+    private function getMode($id)
+    {
+        foreach ($id as $data){
+            $valueArray[] = $data->getBody();
+        }
+        $values = array_count_values($valueArray); 
+        $mode = array_search(max($values), $values);
+        return $mode;
+    }
+    
+    private function getPortentages($id) //FALTA POR ACABAR
+    {
+        foreach ($id as $data){
+            $valueArray[] = $data->getBody();
+        }
+        $count = array_count_values($valueArray); 
+        $total = array_sum($count);
+        
+        foreach ($count as &$data)
+        {
+            $data /= $total;
+        }
+
+        return $count;
+    }
+    
+    private function getTextResponses($id)
+    {
+        foreach ($id as $data){
+            $text[] = $data->getBody();
+        }
+        return $text;
+    }
 }
+
+
